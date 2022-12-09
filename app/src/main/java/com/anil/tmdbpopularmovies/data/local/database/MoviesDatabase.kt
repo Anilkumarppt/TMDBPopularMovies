d30ebc189c9b24321a408f2859207501442ec4f7
anilkumar.pentapati <anilkumar.pentapati@impressico.com>
2022-12-09 17:22:13 +0530

    New Module version

--- app/src/main/java/com/anil/tmdbpopularmovies/data/local/database/MoviesDatabase.kt
+++ app/src/main/java/com/anil/tmdbpopularmovies/data/local/database/MoviesDatabase.kt
@@ -7,7 +7,7 @@ import com.anil.tmdbpopularmovies.data.local.dao.MovieDao
 import com.anil.tmdbpopularmovies.data.local.dto.CastDto
 import com.anil.tmdbpopularmovies.data.remote.model.Movie
 
-@Database(version = 1, entities = [Movie::class,RemoteKey::class,CastDto::class])
+@Database(version = 2, entities = [Movie::class,RemoteKey::class,CastDto::class])
 abstract class MoviesDatabase :RoomDatabase(){
     abstract fun getMovieDao(): MovieDao
     abstract fun getRemoteKeysDao():RemoteKeyDao
